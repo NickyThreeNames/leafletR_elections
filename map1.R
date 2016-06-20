9e4f82ca61b5a31398d9c33326522f11c7092ba4
NickyThreeNames <nich.cm@gmail.com>
2016-06-19 22:42:45 -0500

    Fixed palette and completed layers

--- map1.R
+++ map1.R
@@ -8,6 +8,8 @@ library("scales")
 
 datafile <- "data/election.csv"
 election <- rio::import(datafile)
+election$Pickup <- as.factor(election$Pickup)
+str(election)
 #mnshape <- "data/L2012-1.shp"
 mnshape2 <- "data/cb_2015_27_sldl_500k.shp"
 #mngeo <- read_shape(file=mnshape)
@@ -30,29 +32,97 @@ mngeo@data$DISTRICT <- as.character(mngeo@data$NAME)
 mnmap <- append_data(mngeo, election, key.shp = "NAME", key.data="District")
 
 qtm(mnmap, "DemBase")
-minPct <- min(c(mnmap@data$MNLEGPERC2014, mnmap@data$MNGOVPERC2014))
-maxPct <- max(c(mnmap@data$MNLEGPERC2014, mnmap@data$MNGOVPERC2014))
+
 mnPalette1 <- colorNumeric(palette = "Blues", domain = mnmap@data$MNLEGPERC2014)
 qtm(shp = mnmap, fill = c("MNLEGPERC2014", "MNGOVPERC2014"), fill.palette = mnPalette1, ncol = 2)
 
-mnPalette <- colorNumeric(palette = "RdBlu", domain = mnmap@data$MNLEGPERC2014)
+mnPalette <- colorNumeric(palette = "RdBu", domain = mnmap@data$MNLEGPERC2014)
+mnPalette <- colorBin(palette = "RdBu", domain = mnmap@data$MNLEGPERC2014,bins = c(0, .4, .45, .5, .55, .6, 1) , pretty=FALSE)
 
 mnpopup <- paste0("<b>","District: ","</b>", mnmap@data$DISTRICT, "<br>",
                   "<b>", "Representative: ", "</b>", mnmap@data$Name, "<br>",
-                  "<b>","Dem Candidate '14: ","</b>", percent(mnmap@data$MNLEGPERC2014), "<br>")
+                  "<b>", "Party: ", "</b>", mnmap@data$Party, "<br>",
+                  "<b>","Dem 2014: ","</b>", percent(mnmap@data$MNLEGPERC2014), "<br>",
+                  "<b>", "Pickup: ", "</b>", mnmap@data$Pickup)
 
 leaflet(mngeo2) %>%
   addProviderTiles("CartoDB.Positron") %>%
   addPolygons(stroke=TRUE, 
               smoothFactor = 0.2,
               weight = 1,
-              fillOpacity = .8, 
+              fillOpacity = .6, 
               popup=mnpopup,
               color= ~mnPalette(mnmap@data$MNLEGPERC2014)
               )
+#section for multi-layered
+minPct <- min(c(mnmap@data$MNLEGPERC2014, mnmap@data$MNGOVPERC2014, mnmap@data$DPI, mnmap@data$DemBase))
+maxPct <- max(c(mnmap@data$MNLEGPERC2014, mnmap@data$MNGOVPERC2014, mnmap@data$DPI, mnmap@data$DemBase))
+paletteLayers <- colorBin(palette = "RdBu", domain = c(minPct, maxPct), bins = c(0, .4, .45, .5, .55, .6, 1) , pretty=FALSE)
+incumbPalette <- colorFactor(palette = c("blue", "grey", "red"), domain = mnmap@data$Pickup)
 
-leaflet(mngeo2) %>%   addPolygons(stroke=TRUE, 
-                                 smoothFactor = 0.5,
-                                 weight = 1,
-                                 fillOpacity = .9,
-                                 fill = TRUE)
+leaflet(mngeo2) %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addPolygons(stroke=TRUE,
+              weight=1,
+              smoothFactor = 0.2,
+              fillOpacity = .75,
+              popup=mnpopup, 
+              color= ~incumbPalette(mnmap@data$Pickup),
+              group="Incumbency"
+  ) %>%
+  addLegend(position="bottomleft", colors=c('blue', 'grey', 'red'), labels=c("Dem", "Incumbent", "Repub"))
+
+mnResultmap <- leaflet(mngeo2) %>% 
+  addProviderTiles("CartoDB.Positron") %>%
+  addPolygons(stroke=TRUE,
+              weight=1,
+              smoothFactor = 0.2,
+              fillOpacity = .75,
+              popup=mnpopup, 
+              color= ~incumbPalette(mnmap@data$Pickup),
+              group="Pickup"
+  ) %>% 
+  addLegend(position="bottomleft", colors=c('blue', 'grey', 'red'), labels=c("Dem", "Incumbent", "Repub"))  %>%
+  
+  addPolygons(stroke=TRUE,
+              weight=1,
+              smoothFactor = 0.2, 
+              fillOpacity = .75, 
+              popup=mnpopup, 
+              color= ~paletteLayers(mnmap@data$MNLEGPERC2014),
+              group="DFL House 2014"
+  ) %>%
+  
+  addPolygons(stroke=TRUE,
+              weight=1,
+              smoothFactor = 0.2, 
+              fillOpacity = .75, 
+              popup=mnpopup, 
+              color= ~paletteLayers(mnmap@data$DPI),
+              group="DPI"
+  ) %>%
+  
+  addPolygons(stroke=TRUE,
+              weight=1,
+              smoothFactor = 0.2, 
+              fillOpacity = .75, 
+              popup=mnpopup, 
+              color= ~paletteLayers(mnmap@data$DemBase),
+              group="Democratic Base"
+  ) %>%
+  
+  addPolygons(stroke=TRUE,
+              weight=1,
+              smoothFactor = 0.2, 
+              fillOpacity = .75, 
+              popup=mnpopup, 
+              color= ~paletteLayers(mnmap@data$MNGOVPERC2014),
+              group="Governor"
+  ) %>%
+  
+  addLayersControl(
+    baseGroups=c("Pickup", "DFL House 2014", "DPI", "Democratic Base", "Governor"),
+    position = "bottomleft",
+    options = layersControlOptions(collapsed = FALSE)
+  ) 
+mnResultmap
